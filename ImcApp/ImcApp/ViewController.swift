76443ab0b79520da825602b4c4b46085adc36010
Giovanni Moratto <morattotrabalho@gmail.com>
2021-10-27 16:29:32 -0300

    Adjusting the layout and connecting elements to code

--- ImcApp/ImcApp/ViewController.swift
+++ ImcApp/ImcApp/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Attributes
+    
+    @IBOutlet weak var tfWeight: UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var ivResult: UIImageView!
+    
+    // MARK: - Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func calculate(_ sender: Any) {
     }
 
 
